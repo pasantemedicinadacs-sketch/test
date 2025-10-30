a2c0431640d322d1a1afb012accb8aa3a04d5917
pasantemedicinadacs-sketch <pasante.medicina.dacs@gmail.com>
2025-10-30 02:31:54 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+// --- service-worker.js ---
+const CACHE_NAME = "recetario-cache-v1";
+const urlsToCache = [
+  "hola.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Archivos cacheados correctamente");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
